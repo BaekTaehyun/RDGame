f55a14c4365ad0d84a8d9b429628929352b34c8b
baektaehyun <39148408+BaekTaehyun@users.noreply.github.com>
2025-12-16 00:37:41 +0900

    Remove _read folder

--- _read/ReBirth/ReBirth.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ReBirth.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReBirth, "ReBirth" );
-
-DEFINE_LOG_CATEGORY(LogReBirth)
